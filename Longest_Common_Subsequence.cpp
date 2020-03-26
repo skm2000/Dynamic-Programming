b02e17afc02b0fcc3de4c45dac27d514188ad87f
Skm2000 <shubhamkumarmondal@gmail.com>
2020-03-26 14:37:18 +0530

    DP

---
+++ Longest_Common_Subsequence.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Lcs(string s1,string s2){
+    int m = s1.length(),n = s2.length();
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0 || j==0) dp[i][j]=0;
+            else if(s1[i-1] == s2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main(){
+    string s1 = "abcdgl";
+    string s2 = "abedfhl";
+    cout<<Lcs(s1,s2)<<"\n";
+}
